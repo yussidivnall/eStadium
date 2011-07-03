b4b9c31d48f536ada6b9421796020aaee792befa
uri <UC.dev.null@gmail.com>
2011-07-03 17:52:25 +0100

    Jumps to Order Activity

--- gen/uc/tjt/estadium/R.java
+++ gen/uc/tjt/estadium/R.java
@@ -23,13 +23,13 @@ public final class R {
     public static final class id {
         public static final int FooterLayout=0x7f050006;
         public static final int HeaderLayout=0x7f050000;
-        public static final int button1=0x7f05000a;
-        public static final int imageView1=0x7f050008;
+        public static final int button1=0x7f050008;
+        public static final int imageView1=0x7f05000a;
         public static final int imageView2=0x7f050001;
-        public static final int imageView3=0x7f050009;
+        public static final int imageView3=0x7f05000b;
         public static final int infoButton=0x7f050005;
         public static final int innerLinear=0x7f050007;
-        public static final int linearLayout1=0x7f05000b;
+        public static final int linearLayout1=0x7f050009;
         public static final int linearLayout2=0x7f050002;
         public static final int promotionsButton=0x7f050004;
         public static final int refreshmentsButton=0x7f050003;
--- src/uc/tjt/estadium/EStadiumActivity.java
+++ src/uc/tjt/estadium/EStadiumActivity.java
@@ -2,11 +2,13 @@ package uc.tjt.estadium;
 
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class EStadiumActivity extends Activity {
     /** Called when the activity is first created. */
@@ -15,17 +17,25 @@ public class EStadiumActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        Button mRefreshmentsButton = (Button)findViewById(R.id.refreshmentsButton);
-        //mRefreshmentsButton.setOnClickListener(new OnClickListener(){
-        /*
+        ImageButton mRefreshmentsButton = (ImageButton)findViewById(R.id.refreshmentsButton);
+        
+        
+        
+        mRefreshmentsButton.setOnClickListener(new OnClickListener(){
+        
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				try{
          		 Intent intent = new Intent(v.getContext(), OrderActivity.class);
-        		 startActivityForResult(intent,0);				
+        		 startActivityForResult(intent,0);
+				}catch(ActivityNotFoundException ane){
+					
+				}
 			}
         	
         });
+        /*
         */
     }
 }
